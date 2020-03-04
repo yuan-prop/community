1f35152e3ba925bc41495daeb89869816505b935
yuan-prop <cumtb_wzy2017@163.com>
2020-03-04 19:10:22 +0800

    init repo

---
+++ src/main/java/majiang/comunity/ComunityApplication.java
@@ -0,0 +1,13 @@
+package majiang.comunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunityApplication.class, args);
+	}
+
+}
