9bdd1c55734d9c1f2777f16811baa4f33894f8c8
yuan-prop <cumtb_wzy2017@163.com>
2020-03-07 18:40:06 +0800

    add flyway migration support

--- src/main/java/majiang/comunity/controller/AuthorizeController.java
+++ src/main/java/majiang/comunity/controller/AuthorizeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -40,6 +41,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
+                           HttpServletRequest request,
                            HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -60,6 +62,7 @@ public class AuthorizeController {
             userMapper.insert(user);
             response.addCookie(new Cookie("token", token));
 
+//            request.getSession().setAttribute("user", user);
             System.out.println(user.getName());
             return "redirect:/";
         }else{
