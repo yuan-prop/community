e826c3edd6d075eb9ab37e5e659d99946a48531a
yuan-prop <cumtb_wzy2017@163.com>
2020-03-07 14:21:47 +0800

    add cookie

--- src/main/java/majiang/comunity/controller/IndexController.java
+++ src/main/java/majiang/comunity/controller/IndexController.java
@@ -1,9 +1,13 @@
 package majiang.comunity.controller;
 
+import majiang.comunity.mapper.UserMapper;
+import majiang.comunity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by lenovo on 2020/3/4.
@@ -11,8 +15,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
--- src/main/java/majiang/comunity/mapper/UserMapper.java
+++ src/main/java/majiang/comunity/mapper/UserMapper.java
@@ -3,13 +3,17 @@ package majiang.comunity.mapper;
 import majiang.comunity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by lenovo on 2020/3/6.
  */
 @Mapper
 public interface UserMapper {
-
     @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) VALUES(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
